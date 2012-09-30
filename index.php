4633f8bcad9f7737919cf092b64bfcfde759bc67
LEE JACOB AVITAL <lja5234@cyclops.cs.rit.edu>
2012-09-30 16:12:37 -0400

    Bare bones is now working--the index page will read a json file and display the contents.

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 <?php
 
 	require_once "post.php";
-	$post = new Post($_GET["p"] or "home");
+	$post = new Post($_GET["p"]);
 ?>
 
 <html>
@@ -10,15 +10,8 @@
 		<title>Lee Avital</title>
 	</head>
 	<body>
-		<h1>
-		<?php 
-			if($post->exists()){
-				echo "Exists";
-			}else{
-				echo "The post could not be found";
-			}
-			
-		?>
-		</h1>
+		<h1><?php echo $post->title() 	  ?> </h1>
+		<h2><?php echo $post->subtitle(); ?> </h2>
+		<div class="body"><?php echo $post->body(); ?></div>
 	</body>
 </html>
--- post.php
+++ post.php
@@ -5,14 +5,38 @@
 		
 		private $exists;
 		private $text;
-		
+		private $title;
+		private $subtitle;
+
 		function __construct($title = "home"){
 			
 			if(file_exists("docs/$title.json")){
 				$this->exists = true;	
+			
+				try{
+					$file = fopen("docs/$title.json", "r");
+					
+					try{
+						$data = fread($file, filesize("docs/$title.json"));
+						$data = json_decode($data);
+						$this->title = $data->title or $title;
+						$this->subtitle = $data->subtitle;
+						$this->text = $data->text;
+
+					}catch(Exception  $e){
+						echo "error reading the file...";
+					}
+
+				
+				}catch(Exception $e){
+					echo "could not read the file";
+				}
+			
 			}else{
 				$this->exists = false;
 			}
+
+
 
 
 		
@@ -20,8 +44,20 @@
 		}
 
 
-		function exists(){
+		public function exists(){
 			return $this->exists;
+		}
+
+		public function title(){
+			return $this->title;
+		}
+
+		public function subtitle(){
+			return $this->subtitle;
+		}
+
+		public function body(){
+			return $this->text;
 		}
 	
 	}
